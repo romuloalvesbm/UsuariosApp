d5b50b2a2205e882da9f8dccea9717afa686fee0
romuloalvesbm <romuloalves.br@gmail.com>
2024-04-07 09:22:53 -0300

    feat: Configuração do CORS e ambiente de produção

--- UsuariosApp.API/Program.cs
+++ UsuariosApp.API/Program.cs
@@ -7,10 +7,12 @@ builder.Services.AddRouting(options => options.LowercaseUrls = true);
 builder.Services.AddSwaggerDoc();
 builder.Services.AddIdentityContext(builder.Configuration);
 builder.Services.AddJwtBearer(builder.Configuration);
+builder.Services.AddCorsConfig();
 
 var app = builder.Build();
 
 app.UseSwaggerDoc();
+app.UseCorsConfig();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
